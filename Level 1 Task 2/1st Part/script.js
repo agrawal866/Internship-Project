31059e8d79a1d2afcc53819a675e69ed2167eab5
agrawal866 <168573553+agrawal866@users.noreply.github.com>
2025-02-18 02:05:25 +0530

    Add files via upload

---
+++ Level 1 Task 2/1st Part/script.js
@@ -0,0 +1,14 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+    var randomColor = getRandomColor();
+    this.style.backgroundColor = randomColor;
+  });
+  
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
